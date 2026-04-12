74bb1ad11a81cee53498060447a2e7a5a0cf0313
Nithin K P <nithinkp0212@gmail.com>
2026-04-12 19:32:58 +0530

    Deployment ready portfolio

--- app.py
+++ app.py
@@ -1061,7 +1061,6 @@ def delete_highlight(highlight_id):
 def uploads_disabled(_path: str):
     abort(410)
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5000"))
-    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
